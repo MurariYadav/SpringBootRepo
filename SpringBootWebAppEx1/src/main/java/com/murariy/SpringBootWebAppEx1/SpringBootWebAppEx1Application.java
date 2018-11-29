01e02e2c444b6070418504337b1870ad03a2091a
Murari Kumar <murariugi@gmail.com>
2018-11-30 02:10:26 +0530

    Spring web 1st

---
+++ SpringBootWebAppEx1/src/main/java/com/murariy/SpringBootWebAppEx1/SpringBootWebAppEx1Application.java
@@ -0,0 +1,12 @@
+package com.murariy.SpringBootWebAppEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppEx1Application.class, args);
+	}
+}
